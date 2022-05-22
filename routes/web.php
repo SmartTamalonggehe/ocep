d38a63ef083ebb7e18a7698cb9dde47fe92eeb72
SmartTamalonggehe <smartspartacus@gmail.com>
2022-05-22 11:10:18 +0900

    ganti route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user.dashboard.index');
+    return redirect()->route('login');
 })->name('web');
 
 // Route::get('/', function () {
@@ -30,5 +30,5 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
-require __DIR__.'/admin.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/admin.php';
